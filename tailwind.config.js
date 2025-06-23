de029b45c5243a0232675a405121a6e2efbfc5c3
workonlly <workonlly335@gmail.com>
2025-06-23 10:15:41 +0530

    yfyfy

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './public/index.html'],
+  safelist: ['g-hov', 'button-1', 'button-2', 'button-3'],
+};
