c51e6780fddc497450415b389861bde39132ff53
workonlly <workonlly335@gmail.com>
2025-07-04 11:27:06 +0530

    eee

--- src/linko.ts
+++ src/linko.ts
@@ -5,7 +5,7 @@
         We are highly related by our customers
       </p>
       <div class="flex flex-wrap justify-center gap-10">
-        <a href="https://www.upwork.com/ag/lacleodgital/"><img src="../..public/imo/Upwork-2-removebg-preview.png" alt="Upwork" class="h-14 max-w-[100px] object-contain hover:scale-115" /></a>
+        <a href="https://www.upwork.com/ag/lacleodgital/"><img src="../../public/imo/Upwork-2-removebg-preview.png" alt="Upwork" class="h-14 max-w-[100px] object-contain hover:scale-115" /></a>
        <a href="https://www.freelancer.in/u/lacleodigital"> <img src="../../public/imo/freela-removebg-preview.png" alt="Freelancer" class="h-14 max-w-[100px] object-contain  hover:scale-115" /></a>
        <a href="https://clutch.co/profile/lacleodigital"><img src="../../public/imo/2223-removebg-preview.png" alt="Clutch" class="h-14 max-w-[100px] object-contain hover:scale-115" /></a> 
        <a href="https://www.guru.com/freelancers/la-cleo-digital"><img src="../../public/imo/gur-removebg-preview.png" alt="Guru" class="h-14 max-w-[100px] object-contain hover:scale-115" /></a> 
